c7dfbdd9e0fef3e6344734b2932ef9ed48c93b25
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-07-14 10:55:39 +0100

    add:blinks_swap

---
+++ app/api/actions/transfer-sol/const.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+  "GzJj5ubCzEbakT6zrcnArnVeNAGBsEkVx6LLkCEMdBvT" // devnet wallet
+);
+
+export const DEFAULT_SOL_AMOUNT: number = 100;
